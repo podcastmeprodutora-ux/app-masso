d6e32dca578ea6a2bebdb08d3e5a9944cb76834a
podcastmeprodutora-ux <podcastmeprodutora@gmail.com>
2026-03-21 20:22:25 -0300

    Add basic authentication configuration

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from models import Cliente, Produto, Servico, Pacote, Venda, Atendimento, Contro
 from datetime import datetime, timedelta
 
 app = Flask(__name__)
+app.config['BASIC_AUTH_USERNAME'] = 'mlopes'
+app.config['BASIC_AUTH_PASSWORD'] = 'LuaGorda5@s'
+app.config['BASIC_AUTH_FORCE'] = True
+basic_auth = BasicAuth(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///masso.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'super_secret_key'
