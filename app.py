e054cd4f55e172116b96303bb3133e460110e164
podcastmeprodutora-ux <podcastmeprodutora@gmail.com>
2026-03-21 21:09:21 -0300

    Refactor database URI configuration for flexibility
    Updated database URI configuration to support dynamic database selection based on environment variable.

--- app.py
+++ app.py
@@ -6,11 +6,22 @@ from datetime import datetime, timedelta
 from flask_basicauth import BasicAuth
 
 app = Flask(__name__)
+
+# Configuração de Autenticação
 app.config['BASIC_AUTH_USERNAME'] = 'mlopes'
 app.config['BASIC_AUTH_PASSWORD'] = 'LuaGorda5@s'
 app.config['BASIC_AUTH_FORCE'] = True
 basic_auth = BasicAuth(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///masso.db'
+
+# --- CONFIGURAÇÃO DO BANCO DE DADOS DINÂMICO ---
+# Ele tenta pegar a chave do Render. Se não existir, usa o sqlite local.
+uri = os.getenv("DATABASE_URL")
+if uri and uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = uri or 'sqlite:///masso.db'
+# -----------------------------------------------
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'super_secret_key'
 
